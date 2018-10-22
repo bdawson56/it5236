8e754787651a58f2bcccc7b66030f0467d39be5b
bdawson56 <42593761+bdawson56@users.noreply.github.com>
2018-10-21 21:03:19 -0400

    Added credentials

---
+++ website/include/credentials.php
@@ -0,0 +1,23 @@
+<?php
+/*
+class Credentials {
+
+    // Declare the credentials to the database
+    public $servername = "localhost";
+    public $serverusername = "root56";
+    public $serverpassword = "bdawson56";
+    public $serverdb = "it5236";
+
+}
+*/
+class Credentials {
+
+    // Declare the credentials to the database
+    public $servername = "it5236dbinstance.cddlmxmuawtn.us-east-1.rds.amazonaws$
+    public $serverusername = "it5236user";
+    public $serverpassword = "nosabotage56";
+    public $serverdb = "it5236db";
+    
+    }
+    
+    ?>
